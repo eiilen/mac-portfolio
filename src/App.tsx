9558fc29c8d3607d2922d8eb68efdf0cc65f4a2d
ei <ei.saltedfish@gmail.com>
2025-12-18 17:01:37 +0700

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import Navbar from "./components/Navbar"
+
 const App = () => {
   return (
-    <h1>hello this is ei</h1>
+    <main>
+      <Navbar />
+    </main>
   )
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,35 @@
+import dayjs from 'dayjs'
+import { navIcons, navLinks } from '../constants'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div>
+            <img src='/images/logo.svg' />
+            <p className='font-bold'>Hua Cheng's Portfolio</p>
+
+            <ul>
+                {navLinks.map((item) => (
+                    <li key={item.id}>
+                        <p>{item.name}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+
+        <div>
+            <ul>
+                {navIcons.map((icon) => (
+                    <li key={icon.id}>
+                        <img src={icon.img} className='icon-hover' alt={`icon-${icon.id}`} />
+                    </li>
+                ))}
+            </ul>
+
+            <time>{dayjs().format('ddd MMM D h:mm A')}</time>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
