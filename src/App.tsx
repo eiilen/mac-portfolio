53540e65735bb4e248de5182bf16e477ff82a637
ei <ei.saltedfish@gmail.com>
2025-12-19 15:42:47 +0700

    add hoc managing window states & imp terminal

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import Dock from "./components/Dock"
 import Navbar from "./components/Navbar"
 import Welcome from "./components/Welcome"
+import Terminal from "./windows/Terminal"
+import gsap from "gsap"
+import { Draggable } from "gsap/Draggable"
+gsap.registerPlugin(Draggable)
 
 const App = () => {
   return (
@@ -8,6 +12,8 @@ const App = () => {
       <Navbar />
       <Welcome />
       <Dock />
+
+      <Terminal />
     </main>
   )
 }
