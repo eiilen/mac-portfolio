bdc686a70050568107da22584420be6f02f3070e
ei <ei.saltedfish@gmail.com>
2026-01-06 09:36:30 +0700

    update path

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/mac-portfolio'
+  base: '/mac-portfolio/'
 })
